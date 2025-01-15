6084a7dfb416f5702df1248ffcb599c502dbec12
Johan <johanjuulj@gmail.com>
2025-01-15 10:47:16 +0100

    added urls for navbar links

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -3,6 +3,9 @@ from . import views  # Import views from the core app
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
+    path("about/", views.AboutView.as_view(), name="about"),
+    path("roadmap/", views.RoadMapView.as_view(), name="roadmap"),
+    path("vision/", views.VisionView.as_view(), name="vision"),
 ]
 
 
